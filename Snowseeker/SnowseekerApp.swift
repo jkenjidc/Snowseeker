392120c7f96b90503c3ba70fa629f5981aea1226
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-08-28 14:14:52 -0400

    Initial Commit

---
+++ Snowseeker/SnowseekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowseekerApp.swift
+//  Snowseeker
+//
+//  Created by Kenji Dela Cruz on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowseekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
